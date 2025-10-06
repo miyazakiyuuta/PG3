b9605f0a5f558b26c40462247eaf371406778eb9
宮﨑遊太 <k024g0091@g.neec.ac.jp>
2025-10-06 12:20:06 +0900

    01-02修正,02-01完了

---
+++ 02-01/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+template<typename Type>
+Type Min(Type a, Type b){
+	return (a < b) ? a : b;
+}
+
+int main(){
+
+	printf("%d\n", Min<int>(3, 5));
+	printf("%f\n", Min<float>(4.2f, 3.2f));
+	printf("%f\n", Min<double>(3.5, 2.1));
+
+	return 0;
+}
