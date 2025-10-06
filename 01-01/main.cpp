edeb45500aad559df9c868bc5453b67f2700c4cc
宮﨑遊太 <k024g0091@g.neec.ac.jp>
2025-10-06 10:53:29 +0900

    01-01完了

---
+++ 01-01/main.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <list>
+
+using namespace std;
+
+int main() {
+	list<const char*> lst{
+		"Tokyo","Yurakucho","Shimbashi","Hamamatsucho","Tamachi","Shinagawa","Osaki","Gotanda","Meguro",
+		"Ebisu","Shibuya","Harajuku","Yoyogi","Shinjuku","Shin-Okubo","Takadanobaba","Mejiro","Ikebukuro",
+		"Otsuka","Sugamo","Komagome","Tabata","Nippori","Uguisudani","Ueno","Okachimachi","Akihabara","Kanda"
+	};
+	
+	const char* str = "1970";
+	printf("%s\n", str);
+	for(auto itr=lst.begin(); itr!=lst.end(); ++itr) {
+		if (*itr == "Nippori") { printf("\n"); }
+		if (*itr == "Shin-Okubo") { printf("\n"); }
+		if (*itr == "Shinagawa") { printf("\n"); }
+		printf("%s,", *itr);
+	}
+
+	str = "2019";
+	printf("\n%s\n", str);
+	for(auto itr=lst.begin(); itr!=lst.end(); ++itr) {
+		if(*itr == "Nippori") {
+			itr = lst.insert(itr, "Nishi-Nippori");
+			printf("%s,\n", *itr);
+			++itr;
+		}
+		if (*itr == "Shin-Okubo") { printf("\n"); }
+		if (*itr == "Shinagawa") { printf("\n"); }
+		printf("%s,", *itr);
+	}
+
+	str = "2022";
+	printf("\n%s\n", str);
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+		if (*itr == "Nippori") { printf("\n"); }
+		if (*itr == "Shin-Okubo") { printf("\n"); }
+		if (*itr == "Shinagawa") {
+			itr = lst.insert(itr, "Takanawa Gateway");
+			printf("%s,\n", *itr);
+			++itr;
+		}
+		printf("%s,", *itr);
+	}
+
+}
